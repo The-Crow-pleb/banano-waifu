cd82abf79084f14b90deb59ac6addc76fe5bfa12
The-Crow-Pleb <you@example.com>
2021-03-12 08:02:05 -0300

    Improved²

--- src/commands/calculator/bananominer.js
+++ src/commands/calculator/bananominer.js
@@ -1,6 +1,5 @@
 const lang = require('../../utils/languages/languages')
 const math = require('mathjs')
-const pages = require('discord.js-pagination')
 const { MessageEmbed } = require("discord.js")
 
 module.exports = {
@@ -8,9 +7,8 @@ module.exports = {
     description: 'Calcula o quanto de banano você vai receber do f@h',
     run: async(client, message, args) => {
 
-        const {guild} = message
-        let PPD = args[0]; let currency = args[1];
-        let scope; let PPDAlt; let PPDUtil;
+        const {guild} = message; const get = client.crypto
+        let PPD = args[0]; let scope; let PPDAlt; let PPDUtil;
         
         if(PPD > 300000) {
             scope = {fExp: 5E-05, ppd:PPD, sExp:112.13}
@@ -24,50 +22,44 @@ module.exports = {
 
         if(!PPD) {
             return message.reply(lang(guild, "miner"))
-        } else if(!currency) {
-            
+        } 
+
+        const banano = await get.coins.markets({vs_currency: 'btc', ids: 'banano'})
+        let data = banano.data
+        let btc = await data.map(x => x.current_price);
+
+        let usd = await get.coins.markets({vs_currency: 'usd', ids: 'banano'})
+        let bananoUSD =  PPDAlt * usd.data.map(x => x.current_price)
+
+        const nano = await get.coins.markets({vs_currency: 'usd', ids: 'nano'})
+        let nanoData = nano.data.map(x => x.current_price)
+        let nanoPrice = bananoUSD / nanoData
+
+        if(data.error === 'invalid vs_currency') {
+
+            const errorEmbed = new MessageEmbed()
+                .setColor('#ff0000')
+                .setAuthor(guild.name, guild.iconURL({dynamic: true}))
+                .setTitle(lang(guild, "crypt_err"))
+                .addField(lang(guild, "crypt_err2"), `\`\`\`Invalid Currency\`\`\``)
+            return message.reply(errorEmbed)
+
+        } else if(banano.success === true) {
+
             message.reply(`${lang(guild, "miner_2")} ${PPD}${lang(guild, "miner_2b")}`).then(async(msg) => {
                 const dlt = msg.delete({timeout: 3000})
                 message.channel.startTyping()
-                const calculo = new MessageEmbed()
+                const calculoUm = new MessageEmbed()
                     .setAuthor(guild.name, 'https://cdn.discordapp.com/emojis/815713271918231564.gif?v=1')
                     .addFields(
-                        {name: `${lang(guild, "miner_3")}: ${PPDUtil}`, value: `\`\`\`diff\n-${lang(guild,"miner_diff1")} ${PPD}\n\n+${lang(guild, "miner_diff2")} ${PPDAlt} Bananos\n\n---${lang(guild, "miner_auth")} ZZMthesurand#4965\`\`\``}
+                        {name: `${lang(guild, "miner_3")} ${PPDUtil}`, value: `\`\`\`diff\n-${lang(guild,"miner_diff1")} ${PPD}\n\n+${lang(guild, "miner_diff2")} ${PPDAlt} Bananos\n\n+${lang(guild, "miner_diff3")} ${btc * PPDAlt} Sats\n\n+${lang(guild, "miner_diff4")} ${nanoPrice}\n\n+${lang(guild, "miner_diff5")} ${bananoUSD}\n\n---${lang(guild, "miner_auth")}\`\`\``}
                     )
                     .setColor("#FA5407")
+                    .setFooter(`${lang(guild, "data_prov")} ${data.map(x=>x.last_updated)}`)
                 await dlt; message.channel.stopTyping()
-                message.reply(calculo)
+                message.reply(calculoUm)
             })
-
-        } else {
-            const get = client.crypto
-            const banano = await get.coins.markets({vs_currency: currency, ids: 'banano'})
-            let data = banano.data
-
-            if(data.error === 'invalid vs_currency') {
-                const errorEmbed = new MessageEmbed()
-                    .setColor('#ff0000')
-                    .setAuthor(guild.name, guild.iconURL({dynamic: true}))
-                    .setTitle(lang(guild, "crypt_err"))
-                    .addField(lang(guild, "crypt_err2"), `\`\`\`Invalid Currency\`\`\``)
-                return message.reply(errorEmbed)
-            } else if(banano.success === true) {
-                let price = await data.map(x => x.current_price);
-
-                message.reply(`Certo, seu PPD é de ${PPD}, irei calcular para você, me dê um segundo. . .`).then(async(msg) => {
-                    const dlt = msg.delete({timeout: 3000})
-                    message.channel.startTyping()
-                    const calculoUm = new MessageEmbed()
-                        .setAuthor(guild.name, 'https://cdn.discordapp.com/emojis/815713271918231564.gif?v=1')
-                        .addFields(
-                            {name: `${lang(guild, "miner_3")}: ${PPDUtil}`, value: `\`\`\`diff\n-${lang(guild,"miner_diff1")} ${PPD}\n\n+${lang(guild, "miner_diff2")} ${PPDAlt} Bananos\n\n+${lang(guild, "miner_diff3")} ${price * PPDAlt} ${currency.toUpperCase()}\n\n---${lang(guild, "miner_auth")} ZZMthesurand#4965\`\`\``}
-                        )
-                        .setColor("#FA5407")
-                    await dlt; message.channel.stopTyping()
-                    message.reply(calculoUm)
-                })
-
-            }
         }
+        
     }
 }
--- src/events/ready/ready.js
+++ src/events/ready/ready.js
@@ -3,7 +3,7 @@ module.exports = async(client) => {
     const {loadLangs} = require('../../utils/languages/languages')
     const db = require('../../configs/dbs/dbLogin');
     
-    db.then(() => console.log(`${client.user.username} se conectou à DB!`)).catch(err => console.log(err))
+    db.then(() => console.log(`${client.user.username} bananoloaded into DB!`)).catch(err => console.log(err))
     console.log(`${client.user.username} Bananoloaded!`)
 
     loadLangs(client)

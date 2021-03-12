c17e75ebebe1be94508a38fda6655e9fbc800980
The-Crow-Pleb <you@example.com>
2021-03-12 19:58:46 -0300

    Status changes

--- src/events/ready/ready.js
+++ src/events/ready/ready.js
@@ -7,8 +7,19 @@ module.exports = async(client) => {
     console.log(`${client.user.username} Bananoloaded!`)
     const crypto = client.crypto
     const banano = await crypto.coins.markets({vs_currency: 'sats', ids: 'banano'})
-    let price = Math.floor(banano.data.map(x => x.current_price))
-    client.user.setActivity(`${price} Sats = 1 Ban!`, {type: "LISTENING"})
+    let price1 = Math.floor(banano.data.map(x => x.current_price)); let price2 = Math.floor(banano.data.map(x => x.current_price));
 
+    client.user.setActivity(`${price1} Sats = 1 Ban!`, {type: "LISTENING"})
+    let activNum = 0
+    setInterval(() => {
+        if(activNum === 0) {
+            client.user.setActivity(`${price2} Sats = 1 Ban!`, {type: "LISTENING"})
+            activNum = 1
+        } else if (activNum === 1) {
+            client.user.setActivity(`${price1} Sats = 1 Ban!`, {type: "LISTENING"})
+            activNum = 0
+        }
+    }, 300000);
+    
     loadLangs(client)
 }

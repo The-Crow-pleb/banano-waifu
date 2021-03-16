e8bb0dfa68324efce90ce8d21772dad453ffb227
The-Crow-Pleb <you@example.com>
2021-03-16 12:06:09 -0300

    Updated Permissions

--- src/events/message/message.js
+++ src/events/message/message.js
@@ -6,7 +6,7 @@ module.exports = async(client, message) => {
 
     if(message.channel.type === 'dm') {
         return message.reply("I am not able to execute commands in DMs!")
-    } else if(message.guild.me.hasPermission("SEND_MESSAGES" && "EMBED_LINKS" && "ADD_REACTIONS")) {
+    } else if(message.guild.me.hasPermission("SEND_MESSAGES" && "EMBED_LINKS" && "ADD_REACTIONS" && "MANAGE_MESSAGES")) {
          if(message.content.startsWith(PREFIX)) {
             const [cmdName, ...cmdArgs] = message.content
                 .slice(PREFIX.length)
@@ -16,7 +16,7 @@ module.exports = async(client, message) => {
             else return
         }
     } else {
-        message.author.send("*English:**\n\nI am missing Permissions to speak, add reactions or send embed links, please, check my permissions or invite me again using the link below:\nhttps://discord.com/oauth2/authorize?client_id=819860981320253470&scope=bot&permissions=347200")
+        message.author.send("*English:**\n\nI am missing Permissions to speak, add reactions, manage messages (I need those to edit and delete my own messages) or send embed links, please, check my permissions or invite me again using the link below:\nhttps://discord.com/oauth2/authorize?client_id=819860981320253470&scope=bot&permissions=347200")
     }
 
 

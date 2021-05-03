f56fc2e78d9f0cc294615adffbbaad221446588e
The-Crow-Pleb <you@example.com>
2021-05-03 18:59:41 -0300

    Correction & Unpaid Balances Calc.

--- src/commands/API/banano-miner.js
+++ src/commands/API/banano-miner.js
@@ -1,8 +1,8 @@
 const { MessageEmbed } = require('discord.js');
-const fetch = require('node-fetch')
-const moment = require('moment')
+const fetch = require('node-fetch'); const moment = require('moment')
+const math = require('mathjs')
 module.exports = {
-    aliases: [],
+    aliases: ['bnm'],
     description: 'Retrieves data from the BananoMiner API',
     run: async(client, message, args) => {
 
@@ -32,18 +32,18 @@ module.exports = {
             if(resMiner.error) {
                 const embedError = new MessageEmbed()
                     .setColor("#ac0404")
-                    .setTitle(`Error!`)
+                    .setTitle(`Banano Miner Error!`)
                     .setDescription(`\`\`\`diff\n-${resMiner.error}\`\`\``)
                 message.reply(embedError); message.channel.stopTyping(); return 
             } 
             // Reading the API map
-            let payment = resMiner.payments.map(x => x.amount); let wus = resMiner.payments.map(x => x.work_units); let scoreMiner = resMiner.payments.map(x => x.score)
+            let payment = resMiner.payments.map(x => x.amount); let wus = resMiner.payments.map(x => x.work_units); let scoreMined = resMiner.payments.map(x => x.score)
 
             //Retrieving the User Info
             const user = resMiner.user.name; const userId = resMiner.user.id; const createdAt = moment(resMiner.user.created_at).locale('pt-br').format('L')
 
             //Formatting the payments, wus and score
-            let totalPayment; let totalWusMiner; let totalScoreMiner;
+            let totalPayment; let totalWusMined; let totalScoreMined;
             if (payment.length > 1) {
                 let sum = 0
                 for(let payments in payment) {
@@ -51,36 +51,34 @@ module.exports = {
                 }
                 totalPayment = sum
             } else totalPayment = payment[0]
-            if(wus.length > 1) {
-                let sum = 0
-                for(let work_units in wus) {
-                    sum += wus[work_units]
-                }
-                totalWusMiner = sum
-            } else totalWusMiner = wus[0]
-            if(scoreMiner.length > 1) {
-                let sum = 0
-                for(let scoreTotal in scoreMiner) {
-                    sum += scoreMiner[scoreTotal]
-                }
-                totalScoreMiner = sum
-            } else totalScoreMiner = scoreMiner[0]
-            if(totalPayment===undefined) totalPayment = 0; if(totalWusMiner===undefined) totalWusMiner = 0; if(totalScoreMiner===undefined) totalScoreMiner = 0 
+
+            if(payment===undefined) totalPayment = 0; 
+            if(wus===undefined) totalWusMined = 0; else totalWusMined = wus[0]
+            if(scoreMined===undefined) totalScoreMined = 0; else totalScoreMined = scoreMined[0]
             
             //------------------------------------------------------------Folding @ Home Report ---------------------------------------------------------------//
             //Retrieving info from the Map and others
             const fetchedFAH = await fetch(fah)
             const bridge2 = await fetchedFAH
             const resFAH = await bridge2.json()
+            if(!resFAH.teams) return message.reply('This ID has 0 points/wus completed for team banano!').then(message.channel.stopTyping());
             let fahWus = resFAH.teams.map(x => x.wus); if(fahWus === undefined) fahWus = 'Error'
             let fahPoints = resFAH.teams.map(x => x.score); if(fahPoints === undefined) fahPoints = 'Error'
             let team = resFAH.teams.map(x => x.team); if(team === undefined) team = 'Error'
             let teamName = resFAH.teams.map(x => x.name); if(teamName === undefined) teamName = 'Error'
             let lastWu = resFAH.teams.map(x => x.last); if(lastWu === undefined) lastWu = 'Error'
             
+            
+            //----------------------------------------------------------If there's unpaid points etc-----------------------------------------------------------||
+
+            let missingPayout = fahPoints - totalScoreMined
+            scope = {fExp: 0.35, sExp: (missingPayout/2), tExp: 0.44}
+            let missingBananos = math.evaluate('fExp * sExp ^ tExp', scope)
+            let missingReport = `+Missing payment in points: ${missingPayout}\n\n+Missing payment in bananos (estimated): ${missingBananos}`
+
             //------------------------------------------------------ Report Message -----------------------------------------------------------------------//
 
-            let BananoMinerReport = `+Account: ${user}\n\n+User ID: ${userId}\n\n+Created at: ${createdAt}\n\n+Total Payment in Bananos: ${totalPayment} Bananos\n\n+Total WUS: ${totalWusMiner} Wus\n\n+Total Score: ${totalScoreMiner} Points`
+            let BananoMinerReport = `+Account: ${user}\n\n+User ID: ${userId}\n\n+Created at: ${createdAt}\n\n+Total Payment in Bananos: ${totalPayment} Bananos\n\n+Total WUS: ${totalWusMined} Wus\n\n+Total Score: ${totalScoreMined} Points`
             let fahReport = `-Total WUS: ${fahWus}\n\n-Last Work Unit: ${lastWu}\n\n-Total Points: ${fahPoints}\n\n-At Team: ${teamName} - ${team}`
 
             const report = new MessageEmbed()
@@ -91,6 +89,7 @@ module.exports = {
                     {name: 'F@H Report', value: `\`\`\`diff\n${fahReport}\`\`\``}
                 )
                 .setFooter('Made by Tocka Waifu for the Banano Community.')
+            if(missingPayout !== 0) report.addField('Missing Payments:', `\`\`\`diff\n${missingReport}\`\`\``)
             message.reply(report).then(message.channel.stopTyping()) //aaaaaaand, send!
         } catch (error) {
             message.channel.stopTyping()
